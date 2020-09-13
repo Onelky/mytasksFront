991e510f9b4cdd3073f4554a6edb28ec580496e3
Onelky <1087043@est.intec.edu.do>
2020-09-13 00:41:50 -0400

    Se agrego la estructura de carpetas Se importo Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { SubtaskComponent } from './subtask/subtask.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    HomeComponent,
+    EditTaskComponent,
+    SubtaskComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ClockComponent } from './clock/clock.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [SideBarComponent, ClockComponent, TasksComponent, TaskComponent]
+  ,
+  imports: [
+    BrowserModule],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
